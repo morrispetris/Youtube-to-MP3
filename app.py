24f78bc174d1d4f417aa5727ec96230ac8b32d40
morrispetris <112695708+morrispetris@users.noreply.github.com>
2022-11-30 15:18:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def downloadYT(link):
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
-            'preferredquality': '192',
+            'preferredquality': '64',
         }],
     }
     with youtube_dl.YoutubeDL(information) as music:
